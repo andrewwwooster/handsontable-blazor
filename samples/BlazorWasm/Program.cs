ee078035e4edb4d274d3e53d96428b8f5b5c227f
Andrew Wooster <andrew.wooster@parexel.com>
2024-05-21 21:18:10 -0400

    Include handsontable.js

--- samples/BlazorWasm/Program.cs
+++ samples/BlazorWasm/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Handsontable.BlazorWasm;
+using Handsontable.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ExampleJsInterop>();
 
 await builder.Build().RunAsync();
