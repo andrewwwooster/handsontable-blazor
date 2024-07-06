17da4b94c333e234b9c4e9ac2de6792304c873a9
Andrew Wooster <andrew.wooster@parexel.com>
2024-07-06 19:21:16 -0400

    Comments

--- src/HandsontableBlazor/Callbacks.cs
+++ src/HandsontableBlazor/Callbacks.cs
@@ -2,10 +2,21 @@ using System.Text.Json;
 
 namespace HandsontableBlazor;
 
+/**
+ * This class defines the classes used to capture callback arguments for the core 
+ * HandsontableBlazor methods.
+ * In Javascript, arguments are passed as individual parameters to the callback function.
+ * Instead here we capture these areguments in a single class that is passed to the
+ * callback.  This simplifies the callback signature.
+ */
 public class Callbacks 
 {
     public interface ICallbackArgs {}
 
+    /**
+    * Callback argument for all core Validate methods 
+    * (ValidateCells(), ValidateColumns(), ValidateRows()).
+    */
     public class ValidateArgs : ICallbackArgs
     {
         public ValidateArgs(JsonDocument jdoc)
